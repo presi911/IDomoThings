a95d27e6dd2cb1db47a4cf94801b894629fac9b3
cristian <cristian@DESKTOP-EKRGPEI>
2018-07-07 12:27:10 -0500

    seccion para el hub

---
+++ src/idomothings/Seccion.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package idomothings;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author cristian
+ */
+public class Seccion {
+    private String nombre;
+    private int tope_dispositivo;
+    Lista_dispositivos lista;
+    private int numdisp;
+    public Seccion() {
+    }
+
+    public Seccion(String nombre, int tope_dispositivo) {
+        this.nombre = nombre;
+        this.tope_dispositivo = tope_dispositivo;
+        this.lista=new Lista_dispositivos();
+        numdisp=0;
+    }
+    
+    
+    public void listar_Dispositivo(Dispositivo d){
+        try {
+             if (numdisp < tope_dispositivo) {
+            lista.Adcionar_dispostivo(d);
+            numdisp += 1;
+        }
+        } catch (Exception e) {
+            System.out.println("Esta seccion no puede contener mas dispositivos");
+        }
+       
+        
+    }
+    public void borrar_Dispositivo(Dispositivo d) throws IOException{
+        lista.eliminar_dispositivo(d);
+        numdisp=numdisp-1;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @return the tope_dispositivo
+     */
+    public int getTope_dispositivo() {
+        return tope_dispositivo;
+    }
+
+    /**
+     * @param tope_dispositivo the tope_dispositivo to set
+     */
+    public void setTope_dispositivo(int tope_dispositivo) {
+        this.tope_dispositivo = tope_dispositivo;
+    }
+}
